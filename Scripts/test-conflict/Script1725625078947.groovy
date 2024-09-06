f6f49c813d1c9a09ab8ac865eed48d21e1d38be7
user.pupuk <user.pupuk@PIHC5644.PUPUK-INDONESIA.local>
2024-09-06 19:19:33 +0700

    Test Conflict

--- Scripts/test-conflict/Script1725625078947.groovy
+++ Scripts/test-conflict/Script1725625078947.groovy
@@ -17,5 +17,9 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.acceptAlert()
+WebUI.openBrowser('')
+
+WebUI.click(findTestObject(null))
+
+WebUI.setText(findTestObject(null), '')
 
